e9e854c9fcb9e18a829dc041cf49b8475ad6aa45
khoalebita <khoa_le_dang@bita.jp>
2023-11-03 16:07:26 +0700

    feat(page): deploy for testing useRoute.

--- app.vue
+++ app.vue
@@ -1,5 +1,69 @@
+<script setup lang="ts">
+const route = useRoute()
+const router = useRouter()
+
+const options = [
+  { value: 'Hello', label: 'Hello' },
+  { value: 'World', label: 'World' },
+  { value: 'Goodbye', label: 'Goodbye' },
+]
+
+const selected = ref('Hello')
+
+watch(
+  () => route.fullPath,
+  () => {
+    const query = route.query.selected as string
+
+    if (query || query !== '') {
+      const idx = options.findIndex(({ value }) => value === query)
+      if (idx > -1) {
+        selected.value = query
+      }
+    } else {
+      throw createError({
+        statusCode: 404,
+        message: 'Page not found',
+        fatal: true
+      })
+    }
+  },
+  { immediate: true }
+)
+
+watch(selected, (currentVal) => {
+  router.push(`?selected=${currentVal}`)
+})
+</script>
+
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
+    <p>{{ selected }}</p>
+
+    <form>
+      <select v-model="selected">
+        <option v-for="option in options" :key="option.value" :value="option.value">
+          {{ option.label }}
+        </option>
+      </select>
+    </form>
   </div>
 </template>
+
+<style scoped>
+div {
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
